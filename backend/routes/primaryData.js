55d4f57f81b110b5210863be5fd0ffc312133875
hahmad9 <hahmadd7@gmail.com>
2022-10-05 01:41:22 -0500

    Updated primaryData according to new model
    Added DELETE operator and updated the GETs and UPDATE

--- backend/routes/primaryData.js
+++ backend/routes/primaryData.js
@@ -37,10 +37,14 @@ router.get("/id/:id", (req, res, next) => {
 router.get("/search/", (req, res, next) => { 
     let dbQuery = "";
     if (req.query["searchBy"] === 'name') {
-        dbQuery = { firstName: { $regex: `^${req.query["firstName"]}`, $options: "i" }, lastName: { $regex: `^${req.query["lastName"]}`, $options: "i" } }
+        dbQuery = {org: { $regex: `^${req.query["org"]}`, $options: "i" }, firstName: { $regex: `^${req.query["firstName"]}`, $options: "i" }, lastName: { $regex: `^${req.query["lastName"]}`, $options: "i" } }
     } else if (req.query["searchBy"] === 'number') {
         dbQuery = {
             "phoneNumbers.primaryPhone": { $regex: `^${req.query["phoneNumbers.primaryPhone"]}`, $options: "i" }
+        }
+    } else if (req.query["searchBy"] === 'org') {
+        dbQuery = {
+            org: { $regex: `^${req.query["org"]}`, $options: "i" }
         }
     };
     primarydata.find( 
@@ -78,7 +82,7 @@ router.post("/", (req, res, next) => {
 });
 
 //PUT update (make sure req body doesn't have the id)
-router.put("/:id", (req, res, next) => { 
+router.put("/:id/:org", (req, res, next) => { 
     primarydata.findOneAndUpdate( 
         { _id: req.params.id }, 
         req.body,
@@ -90,6 +94,20 @@ router.put("/:id", (req, res, next) => {
             }
         }
     );
+});
+
+//DELETE an Entree
+router.delete("/:id/:org", (req, res, next) => { 
+    primarydata.findOneAndDelete( 
+        { _id: req.params.id }, 
+        (error, data) => {
+            if (error) {
+                return next(error);
+            } else {
+                res.json(data);
+            }
+        }
+    ).sort({ 'updatedAt': -1 }).limit(10);
 });
 
 module.exports = router;
