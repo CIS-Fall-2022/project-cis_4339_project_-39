1b657fec80af641f0c341bb41fe2ca98ff40654e
hahmad9 <77754216+hahmad9@users.noreply.github.com>
2022-10-06 23:52:13 -0500

    Update primaryData
    Completed all CRUD operations for primaryData

--- backend/routes/primaryData.js
+++ backend/routes/primaryData.js
@@ -18,29 +18,20 @@ router.get("/", (req, res, next) => {
     ).sort({ 'updatedAt': -1 }).limit(10);
 });
 
-//GET single entry by ID
-router.get("/id/:id", (req, res, next) => {
-    primarydata.find( 
-        { _id: req.params.id }, 
-        (error, data) => {
-            if (error) {
-                return next(error);
-            } else {
-                res.json(data);
-            }
-        }
-    );
-});
 
 //GET entries based on search query
 //Ex: '...?firstName=Bob&lastName=&searchBy=name' 
 router.get("/search/", (req, res, next) => { 
     let dbQuery = "";
     if (req.query["searchBy"] === 'name') {
-        dbQuery = { firstName: { $regex: `^${req.query["firstName"]}`, $options: "i" }, lastName: { $regex: `^${req.query["lastName"]}`, $options: "i" } }
+        dbQuery = {org: { $regex: `^${req.query["org"]}`, $options: "i" }, firstName: { $regex: `^${req.query["firstName"]}`, $options: "i" }, lastName: { $regex: `^${req.query["lastName"]}`, $options: "i" } }
     } else if (req.query["searchBy"] === 'number') {
         dbQuery = {
             "phoneNumbers.primaryPhone": { $regex: `^${req.query["phoneNumbers.primaryPhone"]}`, $options: "i" }
+        }
+    } else if (req.query["searchBy"] === 'org') {
+        dbQuery = {
+            org: { $regex: `^${req.query["org"]}`, $options: "i" }
         }
     };
     primarydata.find( 
@@ -60,8 +51,9 @@ router.get("/events/:id", (req, res, next) => {
     
 });
 
-//POST
+//POST an entree
 router.post("/", (req, res, next) => { 
+    console.log(req)
     primarydata.create( 
         req.body,
         (error, data) => { 
@@ -78,7 +70,7 @@ router.post("/", (req, res, next) => {
 });
 
 //PUT update (make sure req body doesn't have the id)
-router.put("/:id", (req, res, next) => { 
+router.put("/:id/:org", (req, res, next) => { 
     primarydata.findOneAndUpdate( 
         { _id: req.params.id }, 
         req.body,
@@ -91,5 +83,21 @@ router.put("/:id", (req, res, next) => {
         }
     );
 });
+
+//DELETE an Entree
+router.delete("/:id/:org", (req, res, next) => { 
+    primarydata.findOneAndDelete( 
+        { _id: req.params.id }, 
+        (error, data) => {
+            if (error) {
+                return next(error);
+            } else {
+                res.json(data);
+            }
+        }
+    ).sort({ 'updatedAt': -1 }).limit(10);
+});
+
+
 
 module.exports = router;
