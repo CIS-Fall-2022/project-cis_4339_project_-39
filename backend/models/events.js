217fdd99350f3e67b837628dfe2b073698bb5b4c
coderjoshuauzoma <moyochidi99@gmail.com>
2022-10-06 21:17:55 -0500

    Create events.js

---
+++ backend/models/events.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let eventSchema = new Schema({
+    OrgId: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    EventName: {
+        type: String,
+        required: true
+    },
+    Date: {
+        type: Date
+    },
+    ServicesOffered: {
+        type: String
+    },
+    Addresses: [
+        {
+            AddressLine1: String,
+            AddressLine2: String,
+            City: String,
+            Count: String,
+            ZipCode: String
+        }
+    ]
+  }, {
+    collection: 'attendees'
+});
+
+module.exports = mongoose.model('attendee', attendeeSchema)
