3cbe1bd4bf585de737ed0a8d2a7822e9043132de
Adedeji21 <112915891+Adedeji21@users.noreply.github.com>
2022-11-19 22:37:57 -0600

    improved table a little

--- frontend/src/components/dashboard.vue
+++ frontend/src/components/dashboard.vue
@@ -1,11 +1,10 @@
-
 <template>
   <main>
     <div>
       <h1 class="font-bold text-4xl text-red-700 tracking-widest text-center mt-10">Dashboard</h1>
     </div>
     <div>
-      <ChartViewVue/>
+      <ChartViewVue :labels="labels" :attendees="attendees" />
     </div>
     <div class="px-10 pt-20">
       <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10">
@@ -82,16 +81,14 @@
         <table class="min-w-full shadow-md rounded">
           <thead class="bg-gray-50 text-xl">
             <tr>
-              <th class="p-4 text-left">Clients Name</th>
               <th class="p-4 text-left">Event Name</th>
-              <th class="p-4 text-left">Event Date </th>
+              <th class="p-4 text-left">Event Attendees </th>
             </tr>
           </thead>
           <tbody class="divide-y divide-gray-300">
-            <tr @click="editClient(client._id)" v-for="client in queryData" :key="client._id">
-              <td class="p-2 text-left">{{ client.firstName + " " + client.lastName }}</td>
-              <td class="p-2 text-left">{{ event?.eventName??"unknown"}}</td>
-              <td class="p-2 text-left">{{ formattedDate(event?.date) }}</td>
+            <tr @click="editEvent(query._id)" v-for="query in queryData" :key="query._id">
+              <td class="p-2 text-left">{{ query?.eventName??"unknown"}}</td>
+              <td class="p-2 text-left">{{ query?.totalAttendees??0 }}</td>
             </tr>
           </tbody>
         </table>
@@ -102,12 +99,13 @@
 <script>
 import axios from "axios";
 import ChartViewVue from "../views/ChartView.vue";
-import { DateTime } from "luxon";
 export default {
   components:{ChartViewVue},
   data() {
     return {
       queryData: [],
+      labels:[],
+      attendees:[],
       //Parameter for search to occur
       searchBy: "",
       firstName: "",
@@ -117,16 +115,48 @@ export default {
     };
   },
   mounted() {
-    let apiURL = import.meta.env.VITE_ROOT_API + `/primaryData/`;
-    console.log(apiURL)
-    axios.get(apiURL).then((resp) => {
-      this.queryData = resp.data;
-    });
+    this.fetchData()
     window.scrollTo(0, 0);
   },
   methods: {
-    formattedDate(datetimeDB) {
-      return DateTime.fromISO(datetimeDB).plus({ days: 1 }).toLocaleString();
+    async fetchData() {
+      try {
+        this.error = null;
+        this.loading = true;
+        const url =import.meta.env.VITE_ROOT_API + `/eventsData/totalAttendees`;
+        const response = await axios.get(url);
+        //"re-organizing" - mapping json from the response
+        console.log(response)
+        const labels = response.data.map((item) => item["eventName"]);
+        const attendees = response.data.map((item) => item["totalAttendees"]);
+        this.queryData = response.data.map(item => ({eventName: item["eventName"], attendees: item["totalAttendees"]}))
+        for (const i in labels) {
+          this.labels[i] = labels[i]
+        }
+        for (const i in attendees) {
+          this.attendees[i] = attendees[i]
+        }
+      } catch (err) {
+        if (err.response) {
+          // client received an error response (5xx, 4xx)
+          this.error = {
+            title: "Server Response",
+            message: err.message,
+          };
+        } else if (err.request) {
+          // client never received a response, or request never left
+          this.error = {
+            title: "Unable to Reach Server",
+            message: err.message,
+          };
+        } else {
+          // There's probably an error in your code
+          this.error = {
+            title: "Application Error",
+            message: err.message,
+          };
+        }
+      }
     },
     handleSubmitForm() {
       let apiURL = "";
@@ -139,9 +169,9 @@ export default {
           import.meta.env.VITE_ROOT_API +
           `/primaryData/search/?phoneNumbers.primaryPhone=${this.phoneNumber}&searchBy=number`;
       }
-      axios.get(apiURL).then((resp) => {
-        this.queryData = resp.data;
-      });
+      // axios.get(apiURL).then((resp) => {
+      //  this.queryData = resp.data;
+      // });
     },
     clearSearch() {
       //Resets all the variables
@@ -152,170 +182,14 @@ export default {
 
       //get all entries
       let apiURL = import.meta.env.VITE_ROOT_API + `/primaryData/`;
-      axios.get(apiURL).then((resp) => {
-        this.queryData = resp.data;
-      });
+      // axios.get(apiURL).then((resp) => {
+      // this.queryData = resp.data;
+      // });
     },
-    editClient(clientID) {
-      this.$router.push({ name: "updateclient", params: { id: clientID } });
+    editEvent(eventID) {
+      this.$router.push({ name: "eventdetails", params: { id: eventID } });
     },
   },
 };
 </script>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<!-- 
-<template>      
-
-  <main>
-    <div>
-      <h1 class="font-bold text-4xl text-red-700 tracking-widest text-center mt-10">Welcome to the Dashboard</h1>
-
-      
-      
-      <table class="w3-table w3-striped w3-bordered">
-
-        <thead>
-          <tr>
-              <th>First Name</th>
-              <th>Middle Name</th>
-              <th>Last Name</th>
-              <th>Email</th>
-              <th>Phone Number</th>
-              <th>Alternative Phone Number</th>
-              <th>Address Line 1</th>
-              <th>Address Line 2</th>
-              <th>City</th>
-              <th>County</th>
-              <th>Zip Code</th>           
-          </tr>
-        </thead>
-        <body>
-
-          <tr v-for="user_alias in User">
-            <td>{{user_alias.first_name}}</td>
-            <td>{{user_alias.middle_name}}</td>
-            <td>{{user_alias.email}}</td>
-            <td>{{user_alias.phonenumber}}</td>
-            <td>{{user_alias.alternativephonenumber}}</td>
-            <td>{{user_alias.address_line1}}</td>
-            <td>{{user_alias.address_line2}}</td>
-            <td>{{user_alias.city}}</td>
-            <td>{{user_alias.county}}</td>
-            <td>{{user_alias.zipcode}}</td>   
-          </tr>
-        </body>
-      </table>
-      
-
-      <button class="brn btn-large btn-block btn-sucess full-width">Add Client</button>
-
-
-
-
-    </div>
-  </main>
-
-</template>
-
-<script>
-/* eslint-disable */
-
-import axios from 'axios';
-export default {
-  name:'user', 
-  data() {
-    return {
-      User: [],
-
-    }
-    
-  },
-    mounted() {
-    axios.get('http://127.0.0.1:5173/intakeform')
-      .then((response) => {
-        console.log(response.data);
-        this.User = response.data;
-
-      })
-      .catch((error) => {
-          console.log(error);
-      });
-  },
-}
-</script>
-
-form{
-  background-color: #00d9ff;
-  border-radius: 2px;
-  box-shadow: 0 1px 1px #ccc;
-  width: 400px;
-  padding: 35px 60px;
-  margin: 50px auto;
-}
-
-<script>
-export default {
-  methods: {
-    routePush(routeName) {
-      this.$router.push({ name: routeName });
-    },
-  },
-};
-</script> -->
--- frontend/src/views/ChartView.vue
+++ frontend/src/views/ChartView.vue
@@ -19,58 +19,21 @@ export default {
   components: {
     BarChart,
   },
-  data() {
-    return {
-      labels: [],
-      attendees: [],
-    };
-  }, methods: {
-    async fetchData() {
-      try {
-        this.error = null;
-        this.loading = true;
-        const url =import.meta.env.VITE_ROOT_API + `/eventsData/totalAttendees`;
-        const response = await axios.get(url);
-        //"re-organizing" - mapping json from the response
-        console.log(response)
-        const labels = response.data.map((item) => item["eventName"]);
-        const attendees = response.data.map((item) => item["totalAttendees"]);
-
-        for (const i in labels) {
-          this.labels[i] = labels[i]
-        }
-        for (const i in attendees) {
-          this.attendees[i] = attendees[i]
-        }
-      } catch (err) {
-        if (err.response) {
-          // client received an error response (5xx, 4xx)
-          this.error = {
-            title: "Server Response",
-            message: err.message,
-          };
-        } else if (err.request) {
-          // client never received a response, or request never left
-          this.error = {
-            title: "Unable to Reach Server",
-            message: err.message,
-          };
-        } else {
-          // There's probably an error in your code
-          this.error = {
-            title: "Application Error",
-            message: err.message,
-          };
-        }
-      }
-      this.loading = false;
+  props: {
+    labels: {
+      type: Array,
+      default: () => []
     },
-  },
+    attendees: {
+      type: Array,
+      default: () => []
+    }
+     
+  }, 
+    
   
-  mounted() {
-    // call the totalAttendees endpoint
-    this.fetchData()
-  },
+  
+ 
 };
 </script>
 
